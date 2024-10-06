dc3de2b4758926085193b98fb8fb97631762d795
Rupanjan Bhattacharyya <96675641+RupanjanBhattacharyya@users.noreply.github.com>
2024-10-06 11:42:16 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,17 +1,12 @@
 import axios from 'axios';
-import Home from '../components/Home';
-try {
-      const response = await axios.get(`http://152.67.176.72:8081/userauth?username=${username}&password=${password}`);
+const BASE_URL = 'http://152.67.176.72:8081/userauth?username=${username}&password=${password}';
 
-      if (response.data["auth"] == true) {
-        // Redirect to dashboard on successful login
-        navigate('Home');
-      } else {
-        // Show error message if credentials are incorrect
-        setError('Invalid username or password');
-      }
-    } catch (err) {
-      console.error(err);
-      setError('An error occurred while trying to log in');
-    }
-  };
+export default axios.create({
+    baseURL: BASE_URL
+});
+
+export const axiosPrivate = axios.get({
+    baseURL: BASE_URL,
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true
+});
