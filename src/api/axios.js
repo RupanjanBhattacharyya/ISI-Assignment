87f7c106868d7aae2e7809ea3d877c0a596d7588
Rupanjan Bhattacharyya <96675641+RupanjanBhattacharyya@users.noreply.github.com>
2024-10-06 11:30:48 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
+import Home from './components/Home';
 try {
       const response = await axios.get(`http://152.67.176.72:8081/userauth?username=${username}&password=${password}`);
 
       if (response.data["auth"] == true) {
         // Redirect to dashboard on successful login
-        navigate('/dashboard');
+        navigate('Home');
       } else {
         // Show error message if credentials are incorrect
         setError('Invalid username or password');
